a64d477202b04eac9cb10540225c881a5921ec1e
Antoinoe <noe.guillaumin@edu.devinci.fr>
2025-12-08 11:18:32 +0100

    Added Escape MiniGame basic scripts

---
+++ Assets/VRMPAssets/MiniGames/MiniGameScripts/Escape/MiniGame_Escape.cs
@@ -0,0 +1,31 @@
+namespace XRMultiplayer.MiniGames
+{
+    public class MiniGame_Escape : MiniGameBase
+    {
+        public override void Start()
+        {
+            base.Start();
+        }
+
+        public override void SetupGame()
+        {
+            base.SetupGame();
+        }
+
+        public override void StartGame()
+        {
+            base.StartGame();
+        }
+
+        public override void UpdateGame(float deltaTime)
+        {
+            base.UpdateGame(deltaTime);
+        }
+
+        public override void FinishGame(bool submitScore = true)
+        {
+            base.FinishGame(submitScore);
+        }
+    }
+}
+

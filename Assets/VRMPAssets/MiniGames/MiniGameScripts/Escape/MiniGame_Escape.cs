06bb06616b443f52df95b8f4d8eab6a31cb68c60
Antoinoe <noe.guillaumin@edu.devinci.fr>
2025-12-10 14:22:01 +0100

    Updated XR Interaction Toolkit. Game starts when 2 players join and go to their position.

--- Assets/VRMPAssets/MiniGames/MiniGameScripts/Escape/MiniGame_Escape.cs
+++ Assets/VRMPAssets/MiniGames/MiniGameScripts/Escape/MiniGame_Escape.cs
@@ -1,7 +1,12 @@
+using Unity.Netcode;
+using UnityEngine;
+
 namespace XRMultiplayer.MiniGames
 {
     public class MiniGame_Escape : MiniGameBase
     {
+        [SerializeField] private Transform redAnchor, blueAnchor;
+
         public override void Start()
         {
             base.Start();
@@ -15,6 +20,8 @@ namespace XRMultiplayer.MiniGames
         public override void StartGame()
         {
             base.StartGame();
+            print("Game Starts!");
+            ResetPlayerPosition();
         }
 
         public override void UpdateGame(float deltaTime)
@@ -26,6 +33,27 @@ namespace XRMultiplayer.MiniGames
         {
             base.FinishGame(submitScore);
         }
+
+        private void ResetPlayerPosition()
+        {
+            var players = NetworkManager.Singleton.ConnectedClientsList;
+
+            if (players.Count >= 1 && redAnchor != null)
+                Teleport(players[0].PlayerObject, blueAnchor);
+
+            if (players.Count >= 2 && blueAnchor != null)
+                Teleport(players[1].PlayerObject, redAnchor);
+
+        }
+
+        private void Teleport(NetworkObject playerObj, Transform anchor)
+        {
+            var t = playerObj.transform;
+
+            t.position = anchor.position;
+            t.rotation = anchor.rotation;
+        }
+
     }
 }
 
